eb6b37bdaad71edafc9a2776a8fd031e22956a01
aarontravass <aaron.travass@hotmail.com>
2023-07-09 16:54:52 -0400

    fix: added title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AppService } from './app.service';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,29 +8,39 @@ import { AppService } from './app.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor(private readonly appService: AppService) {}
-  title = 'frontend';
+  constructor(
+    private readonly appService: AppService,
+    private readonly titleService: Title
+  ) {}
+ 
   search_query = '';
   readonly #per_page = 10;
   page_no = 1;
   result: any = {};
   async ngOnInit() {
+    this.titleService.setTitle('Search Engine')
     // console.log(this.search_query);
     // this.result =await this.appService.search(this.search_query, 1) ;
   }
 
-  async submit(){
-    this.result =await this.appService.search(this.search_query, 1) ;
-    console.log(this.result)
+  async submit() {
+    this.result = await this.appService.search(this.search_query, 1);
+    console.log(this.result);
   }
 
   async next() {
     if ((this.page_no + 1) * this.#per_page >= 100) return;
-    this.result = await this.appService.search(this.search_query, ++this.page_no) 
+    this.result = await this.appService.search(
+      this.search_query,
+      ++this.page_no
+    );
   }
 
   async prev() {
     if ((this.page_no - 1) * this.#per_page <= 0) return;
-    this.result = await this.appService.search(this.search_query, --this.page_no) 
+    this.result = await this.appService.search(
+      this.search_query,
+      --this.page_no
+    );
   }
 }
